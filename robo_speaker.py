dc941ec4925a512a4b9c7d477f437dcf79066cbc
Parth2632 <Partharors2632@gmail.com>
2024-11-15 16:07:27 +0530

    added a condition

--- robo_speaker.py
+++ robo_speaker.py
@@ -4,9 +4,10 @@ def text_to_speech(text):
     command=f'powershell -Command "(New-Object -ComObject SAPI.SpVoice).Speak(\'{text}\')"'
     os.system(command)
 
-while True:
-    text_to_speech("type:")
-    x = input("text:")
-    if x.lower()=="exit":
-        break
-    text_to_speech(x)
+if __name__ == "__main__":
+    while True:
+        text_to_speech("type:")
+        x = input("text:")
+        if x.lower()=="exit":
+            break
+        text_to_speech(x)
